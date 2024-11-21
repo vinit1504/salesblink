06bf86a3361ce52bff76ef3ea92391c833324780
vinit1504 <vinit1222003@gmail.com>
2024-11-21 12:55:56 +0530

    enhance database connection logic; add dotenv for environment variable management and improve logging for connection status

--- server/database/dataBaseConnection.js
+++ server/database/dataBaseConnection.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config()
+// Function to connect to the MongoDB database
 export const DataBaseConnect = async () => {
+  console.log("Connecting to the database" , process.env.MONGO_URI);
+  
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    // Attempt to connect to the MongoDB using the URI from the environment variables
+    await mongoose.connect(process.env.MONGO_URI);
+    
+    // Log a success message if the connection is established
     console.log("Database connection successful");
   } catch (error) {
-    console.error("Database connection failed:", error.message); // Log the error message for better debugging
+    // Log an error message if the connection fails
+    console.log("Database connection failed");
+    
+    // Exit the process with a failure code if the connection can't be established
     process.exit(1);
   }
 };
